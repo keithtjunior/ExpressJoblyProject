67163e9f04d12f9783f96965079d95493c3ca1e4
Keith <keef3d@gmail.com>
2024-03-07 13:37:42 -0500

    test for sqlForPartialUpdate function

---
+++ helpers/sql.test.js
@@ -0,0 +1,32 @@
+const { sqlForPartialUpdate } = require("./sql");
+const { BadRequestError } = require("../expressError");
+
+const data = {
+    name: "C1",
+    numEmployees: 1,
+    logoUrl: "http://c1.img"
+}
+
+const js = {
+    numEmployees: "num_employees",
+    logoUrl: "logo_url"
+}
+
+describe('returns sql indentifiers and values for partial update query', function () {
+    test('works', function () {
+        const sql = sqlForPartialUpdate(data, js);
+        expect(sql).toEqual({
+            setCols: "\"name\"=$1, \"num_employees\"=$2, \"logo_url\"=$3",
+            values: [data.name, data.numEmployees, data.logoUrl],
+        });
+      });
+
+    test('receives no data', function () {
+        try {
+          const sql = sqlForPartialUpdate({}, {});
+          fail();
+        } catch (err) {
+          expect(err instanceof BadRequestError).toBeTruthy();
+        }
+      });
+});
