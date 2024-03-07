1954583a688ad12ea8e6ba68722241c650af29e2
Keith <keef3d@gmail.com>
2024-03-07 12:47:17 -0500

    add comments/documentation for sqlForPartialUpdate funciton

--- helpers/sql.js
+++ helpers/sql.js
@@ -1,6 +1,16 @@
 const { BadRequestError } = require("../expressError");
 
-// THIS NEEDS SOME GREAT DOCUMENTATION.
+/** Convert request-body keys and variable names into identifiers for
+ *  an SQL statement making a partial update. Returns each with the new 
+ *  values to be inserted.
+ *
+ * If req.body object was provided, converts the key values from each object  
+ * into column identifiers for a sql statement. Key/variable names that do not 
+ * match their equivalent column name are updated; ie "logoUrl" => "logo_url". 
+ * The column identifiers and accompanying values are returned as a new object.
+ * 
+ * A BadRequestError is thrown if no req.body data is provided.
+ */
 
 function sqlForPartialUpdate(dataToUpdate, jsToSql) {
   const keys = Object.keys(dataToUpdate);
